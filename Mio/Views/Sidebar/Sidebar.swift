258d1da686c3d60f4009be131ae55648dcd9deec
Sheng Luo <luosheng1986@gmail.com>
2022-08-23 16:23:41 +0800

    Toggle sidebar

--- Mio/Views/Sidebar/Sidebar.swift
+++ Mio/Views/Sidebar/Sidebar.swift
@@ -18,6 +18,16 @@ struct Sidebar: View {
         }
       }
     }
+    .background(.background)
+    .toolbar {
+      ToolbarItem(placement: .status) {
+        Button(action: {
+          NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+        }) {
+          Image(systemName: "sidebar.squares.leading")
+        }
+      }
+    }
   }
 }
 
