0da587c340d267bab36cf58d877958061069e0ac
Sheng Luo <luosheng1986@gmail.com>
2022-08-23 14:35:09 +0800

    Initial commit

---
+++ Mio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mio
+//
+//  Created by Luo Sheng on 2022/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mio/MioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MioApp.swift
+//  Mio
+//
+//  Created by Luo Sheng on 2022/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
