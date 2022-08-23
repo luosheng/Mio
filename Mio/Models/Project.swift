346491c46e05cba25ea6d7db0ef1add338eb4511
Sheng Luo <luosheng1986@gmail.com>
2022-08-23 21:36:10 +0800

    Make project codable

--- Mio/Models/Project.swift
+++ Mio/Models/Project.swift
@@ -9,13 +9,17 @@ import Foundation
 import SwiftTerm
 import Combine
 
-class Project: Identifiable, LocalProcessDelegate {
+class Project: Codable, Identifiable, LocalProcessDelegate {
   var id: UUID
   var name: String
   var command: String
   var directory: String?
   
   var dataPublisher: PassthroughSubject<ArraySlice<UInt8>, Never>
+  
+  private enum CodingKeys : String, CodingKey {
+    case id, name, command, directory
+  }
   
   init(name: String, command: String, directory: String?) {
     self.id = UUID()
@@ -28,6 +32,16 @@ class Project: Identifiable, LocalProcessDelegate {
   
   convenience init(name: String, command: String) {
     self.init(name: name, command: command, directory: nil)
+  }
+  
+  required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = try container.decode(UUID.self, forKey: .id)
+    name = try container.decode(String.self, forKey: .name)
+    command = try container.decode(String.self, forKey: .command)
+    directory = try container.decode(String?.self, forKey: .directory)
+    
+    self.dataPublisher = PassthroughSubject()
   }
   
   func run() {
