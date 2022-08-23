e3a6139b743fd30a07fdd6af20d49a912e2450d0
Sheng Luo <luosheng1986@gmail.com>
2022-08-23 14:40:03 +0800

    Add mocks

---
+++ Mio/Models/Mocks/Mock.swift
@@ -0,0 +1,13 @@
+//
+//  Mock.swift
+//  Mio
+//
+//  Created by Luo Sheng on 2022/8/23.
+//
+
+import Foundation
+
+let mockProjects = [
+  Project(name: "Project A"),
+  Project(name: "Project B")
+]
