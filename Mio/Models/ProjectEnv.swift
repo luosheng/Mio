314691d24cfc4680fb0338988b1452a63ce7b754
Sheng Luo <luosheng1986@gmail.com>
2022-08-25 13:37:25 +0800

    Add some helper functions to convert between model and string

--- Mio/Models/ProjectEnv.swift
+++ Mio/Models/ProjectEnv.swift
@@ -10,4 +10,22 @@ import Foundation
 struct ProjectEnv: Codable {
   var name: String
   var value: String
+  
+  static func parse(str: String) -> [ProjectEnv] {
+    return str.split(separator: ";")
+      .map { $0.trimmingCharacters(in: .whitespaces).split(separator: "=") }
+      .filter { $0.count != 2 }
+      .map { ProjectEnv(
+        name: String($0[0]).trimmingCharacters(in: .whitespaces),
+        value: String($0[1]).trimmingCharacters(in: .whitespaces)
+      ) }
+  }
+  
+  static func toString(envs: [ProjectEnv]) -> String {
+    return envs.map { $0.toString() }.joined(separator: ";")
+  }
+  
+  func toString() -> String {
+    return "\(name.trimmingCharacters(in: .whitespaces))=\(value.trimmingCharacters(in: .whitespaces))"
+  }
 }
