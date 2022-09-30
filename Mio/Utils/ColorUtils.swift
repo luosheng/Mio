6341d2d1ae5ee60fd7e6546383faf3be05cfe5b5
Sheng Luo <luosheng1986@gmail.com>
2022-09-30 17:00:49 +0800

    Use pantonColors

--- Mio/Utils/ColorUtils.swift
+++ Mio/Utils/ColorUtils.swift
@@ -9,21 +9,25 @@ import AppKit
 import Foundation
 import SwiftUI
 
-let PredefinedColors: [[NSColor]] = [
-  [NSColor(red: 0.29, green: 0.68, blue: 1.00, alpha: 1.00), NSColor(red: 0.00, green: 0.39, blue: 0.90, alpha: 1.00)],
-  [NSColor(red: 0.20, green: 0.20, blue: 0.20, alpha: 1.00), NSColor(red: 0.10, green: 0.05, blue: 0.02, alpha: 1.00)],
-  [NSColor(red: 0.76, green: 0.44, blue: 0.87, alpha: 1.00), NSColor(red: 0.48, green: 0.40, blue: 0.82, alpha: 1.00)],
-  [NSColor(red: 0.99, green: 0.35, blue: 0.07, alpha: 1.00), NSColor(red: 1.00, green: 0.28, blue: 0.00, alpha: 1.00)],
-  [NSColor(red: 0.98, green: 0.35, blue: 0.44, alpha: 1.00), NSColor(red: 0.96, green: 0.12, blue: 0.20, alpha: 1.00)],
-  [NSColor(red: 0.05, green: 0.80, blue: 0.37, alpha: 1.00), NSColor(red: 0.01, green: 0.76, blue: 0.60, alpha: 1.00)],
+let pantonColors = [
+  "#D01C1F", // Fiery Red
+  "#CE2D71", // Beetroot Purple Fiery Red
+  "#FF7F04", // Tangelo
+  "#FA9A84", // Peach Pink
+  "#F6D000", // Empire Yellow
+  "#FEC3C7", // Crystal Rose
+  "#3AA845", // Classic Green
+  "#C1DB3C", // Love Bird
+  "#4B81BF", // Blue Perennial
+  "#A0C3D9", // Summer Song
 ]
 
-let randomColors = PredefinedColors.shuffled()
+let randomColors = pantonColors.shuffled()
 
 var index = 0
 
 func getRandomIconBackgroundColor() -> [Color] {
-  let result = randomColors[index].map { Color(nsColor: $0) }
+  let result = Color(nsColor: NSColor(hexString: randomColors[index]))
   index = (index + 1) % randomColors.count
-  return result
+  return [result]
 }
